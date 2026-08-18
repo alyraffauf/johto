9a2fc311ffac80b8aed7cd8b70de02db98f2959b
Aly Raffauf <aly@aly.codes>
2026-08-18 12:49:41 -0400

    fix: permit local k3s nfs mounts

--- nix/hosts/nixos/goldenrod/nfs.nix
+++ nix/hosts/nixos/goldenrod/nfs.nix
@@ -8,8 +8,8 @@ _: {
     services.nfs.server = {
       enable = true;
       exports = ''
-        /mnt/Data 100.64.0.0/10(rw,sync,no_subtree_check,no_root_squash,fsid=0) 10.42.0.0/16(rw,sync,no_subtree_check,no_root_squash,fsid=0) 10.254.2.0/24(rw,sync,no_subtree_check,no_root_squash,fsid=0)
-        /mnt/Media 100.64.0.0/10(rw,sync,no_subtree_check,no_root_squash,fsid=1) 10.42.0.0/16(rw,sync,no_subtree_check,no_root_squash,fsid=1) 10.254.2.0/24(rw,sync,no_subtree_check,no_root_squash,fsid=1)
+        /mnt/Data 127.0.0.1(rw,sync,no_subtree_check,no_root_squash,fsid=0) 100.64.0.0/10(rw,sync,no_subtree_check,no_root_squash,fsid=0) 10.42.0.0/16(rw,sync,no_subtree_check,no_root_squash,fsid=0) 10.254.2.0/24(rw,sync,no_subtree_check,no_root_squash,fsid=0)
+        /mnt/Media 127.0.0.1(rw,sync,no_subtree_check,no_root_squash,fsid=1) 100.64.0.0/10(rw,sync,no_subtree_check,no_root_squash,fsid=1) 10.42.0.0/16(rw,sync,no_subtree_check,no_root_squash,fsid=1) 10.254.2.0/24(rw,sync,no_subtree_check,no_root_squash,fsid=1)
       '';
     };
   };
